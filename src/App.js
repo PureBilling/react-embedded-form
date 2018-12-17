0b7728807b60779a71b106da6029f9e704f549b5
Vadim <v.kirpun@dunice.net>
2018-12-17 09:49:33 +0300

    initialize application
    create project structure
    create two empty main components
    create tools and service folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import Provider from "./components/Provider";
 
-class App extends Component {
+const setup = {
+  'kr-client-domain': 'https://api.payzen.eu',
+  'kr-theme': "classic",
+  'kr-public-key': '69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5'
+};
+
+class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider options={setup}>
+        <div>
+          Some text
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/components/LyraForm.jsx
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class LyraForm extends PureComponent {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+LyraForm.propTypes = {};
+
+export default LyraForm;
