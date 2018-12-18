153b33e3bb96e0544268f70e19bb7eb0d03feaef
Vadim <v.kirpun@dunice.net>
2018-12-18 16:47:58 +0300

    add some tests

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ const setup = {
   'kr-theme': "classic",
   'kr-public-key': '69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5',
   'kr-form-token': '013023XhmLSs67YzwRY7ZV2w18CeyJhIjo5OTAsIm0iOiJFVVIiLCJvIjpudWxsLCJjIjp7ImIiOnsidmkiOnsiZiI6eyJ2YWRDYXJkVHlwZSI6eyJ2YWx1ZSI6IlZJU0EifX19LCJtYyI6eyJmIjp7InZhZENhcmRUeXBlIjp7InZhbHVlIjoiTUFTVEVSQ0FSRCJ9fX0sImFtIjp7ImYiOnsidmFkQ2FyZFR5cGUiOnsidmFsdWUiOiJBTUVYIn19fSwiY2IiOnsiZiI6eyJkZWJpdENyZWRpdCI6eyJ2YWx1ZSI6ImNyZWRpdCJ9fX19fX0b602',
-  // 'kr-placeholder-pan': 'Pan',
-  // 'kr-placeholder-expiry': 'Expiry',
-  // 'kr-placeholder-security-code': 'Security Code',
-  // 'kr-hide-debug-toolbar': 'false',
-  // 'kr-clear-on-error': 'false',
-  // 'kr-post-url-success': '/success',
-  // 'kr-post-url-refused': '/refused',
-  // 'kr-language': 'en-EN',
+  'kr-placeholder-pan': 'Pan',
+  'kr-placeholder-expiry': 'Expiry',
+  'kr-placeholder-security-code': 'Security Code',
+  'kr-hide-debug-toolbar': 'false',
+  'kr-clear-on-error': 'false',
+  'kr-post-url-success': '/success',
+  'kr-post-url-refused': '/refused',
+  'kr-language': 'en-EN',
 };
 setupOptions(setup);
 
