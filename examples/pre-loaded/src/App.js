c4798d10d1a3a3382491c27aa61ff158ff194af2
Vadim <v.kirpun@dunice.net>
2018-12-20 10:36:14 +0300

    finish pre-loaded example

--- examples/pre-loaded/src/App.js
+++ examples/pre-loaded/src/App.js
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
-import LyraForm from "../../../src/components/LyraForm";
-import setupOptions from "../../../src/tools/setupOptions";
+import setupOptions from "./lyra-form/tools/setupOptions";
+import LyraForm from "./lyra-form/LyraForm";
 
 const setup = {
   'kr-client-domain': 'https://api.payzen.eu',
   'kr-theme': "classic",
   'kr-public-key': '69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5',
-  'kr-form-token': 'SET-YOUR-OWN-TOKEN',
+  'kr-form-token': '013023XhmLSs67YzwRY7ZV2w18CeyJhIjo5OTAsIm0iOiJFVVIiLCJvIjpudWxsLCJjIjp7ImIiOnsidmkiOnsiZiI6eyJ2YWRDYXJkVHlwZSI6eyJ2YWx1ZSI6IlZJU0EifX19LCJtYyI6eyJmIjp7InZhZENhcmRUeXBlIjp7InZhbHVlIjoiTUFTVEVSQ0FSRCJ9fX0sImFtIjp7ImYiOnsidmFkQ2FyZFR5cGUiOnsidmFsdWUiOiJBTUVYIn19fSwiY2IiOnsiZiI6eyJkZWJpdENyZWRpdCI6eyJ2YWx1ZSI6ImNyZWRpdCJ9fX19fX0b602',
   'kr-post-url-success': '/success',
   'kr-post-url-refused': '/refused',
   'kr-language': 'en-EN',
@@ -19,7 +19,7 @@ class App extends PureComponent {
   };
 
   handleOpenForm = () => {
-    this.setState(prevState => ({ isFormOpen: prevState.isFormOpen }));
+    this.setState(prevState => ({ isFormOpen: !prevState.isFormOpen }));
   };
 
   render() {
@@ -31,9 +31,9 @@ class App extends PureComponent {
           Open awesome form below
         </button>
         <LyraForm
-          kr-placeholder-pan="Minimal Example Pan"
-          kr-placeholder-expiry="Minimal Example Expiry"
-          kr-placeholder-security-code="Minimal Example Security Code"
+          kr-placeholder-pan="Pre-loaded Example Pan"
+          kr-placeholder-expiry="Pre-loaded Example Expiry"
+          kr-placeholder-security-code="Pre-loaded Example Security Code"
           kr-hide-debug-toolbar="true"
           kr-clear-on-error="false"
           isVisible={isFormOpen}
